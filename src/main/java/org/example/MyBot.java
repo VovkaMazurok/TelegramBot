88d550bb46f300e36aa6e12deb44d49d2f6f86bf
Volodymyre <vova89.89@i.ua>
2024-02-16 00:34:52 +0200

    commit task #3

--- src/main/java/org/example/MyBot.java
+++ src/main/java/org/example/MyBot.java
@@ -20,29 +20,41 @@ public class MyBot extends TelegramLongPollingBot {
         try {
             var message = new SendMessage();
             message.setChatId(chatId);
-
             if (text.equals("/start")) {
                 message.setText("Hello");
-            } else if (text.equals("btc")) {
-                var price = CryptoPrice.spotPrice("BTC");
-                message.setText("BTC price: " + price.getAmount().doubleValue());
-            } else if (text.equals("eth")) {
-                var price = CryptoPrice.spotPrice("ETH");
-                message.setText("ETH price: " + price.getAmount().doubleValue());
-            } else if (text.equals("all")) {
-                var price = CryptoPrice.spotPrice("BTC");
-                var price1 = CryptoPrice.spotPrice("ETH");
-                message.setText("BTC price: " + price.getAmount().doubleValue() + "\n"
-                        + "ETH price: " + price1.getAmount().doubleValue());
             } else {
-                message.setText("Unknow command!");
+                String[] commands = text.split(",");
+                StringBuilder response = new StringBuilder();
+                for (String command : commands) {
+                    command = command.trim();
+                    if (command.equals("btc")) {
+                        var price = CryptoPrice.spotPrice("BTC");
+                        response.append("BTC price: ").append(price.getAmount().doubleValue()).append("\n");
+                    } else if (command.equals("eth")) {
+                        var price = CryptoPrice.spotPrice("ETH");
+                        response.append("ETH price: ").append(price.getAmount().doubleValue()).append("\n");
+                    } else if (command.equals("doge")) {
+                        var price = CryptoPrice.spotPrice("DOGE");
+                        response.append("DOGE price: ").append(price.getAmount().doubleValue()).append("\n");
+                    } else if (text.equals("all")) {
+                        var price = CryptoPrice.spotPrice("BTC");
+                        var price1 = CryptoPrice.spotPrice("ETH");
+                        var price2 = CryptoPrice.spotPrice("DOGE");
+                        message.setText("BTC price: " + price.getAmount().doubleValue() + "\n"
+                                + "ETH price: " + price1.getAmount().doubleValue() + "\n"
+                                + "DOGE price: " + price2.getAmount().doubleValue());
+                    } else {
+                        response.append("Unknown command: ").append(command).append("\n");
+                    }
+                }
+                message.setText(response.toString());
             }
-
             execute(message);
         } catch (Exception e) {
             System.out.println("Error");
         }
     }
+
 
     @Override
     public String getBotUsername() {
