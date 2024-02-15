be0d2e96eb5b3292aa975aa6694c099689e8691e
Volodymyre <vova89.89@i.ua>
2024-02-16 00:05:15 +0200

    commit task #1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        var api = new TelegramBotsApi(DefaultBotSession.class);
+        api.registerBot(new MyBot());
+
+        // it_practice_super_bot
+        // 6882336805:AAHtkGqGRtfbmR-4EJCHN_M1Xccz05qb-TI
     }
 }
--- src/main/java/org/example/MyBot.java
+++ src/main/java/org/example/MyBot.java
@@ -1,2 +1,46 @@
-package org.example;public class MyBot {
+package org.example;
+
+import net.thauvin.erik.crypto.CryptoPrice;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+
+public class MyBot extends TelegramLongPollingBot {
+
+    public MyBot() {
+        super("6882336805:AAHtkGqGRtfbmR-4EJCHN_M1Xccz05qb-TI");
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var chatId = update.getMessage().getChatId();
+        var text = update.getMessage().getText();
+
+        try {
+            var message = new SendMessage();
+            message.setChatId(chatId);
+
+            if (text.equals("/start")) {
+                message.setText("Hello");
+            } else if (text.equals("btc")) {
+                var price = CryptoPrice.spotPrice("BTC");
+                message.setText("BTC price: " + price.getAmount().doubleValue());
+            } else if (text.equals("eth")) {
+                var price = CryptoPrice.spotPrice("ETH");
+                message.setText("ETH price: " + price.getAmount().doubleValue());
+            } else {
+                message.setText("Unknow command!");
+            }
+
+            execute(message);
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "it_practice_super_bot";
+    }
 }
